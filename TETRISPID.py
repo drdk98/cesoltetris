35f828584a08eb6137e7fa0d59c959da50928af2
drdk98 <120593038+drdk98@users.noreply.github.com>
2023-01-10 14:09:32 -0500

    Update TETRISPID.py

--- TETRISPID.py
+++ TETRISPID.py
@@ -56,3 +56,18 @@ class Tetris:
     zoom = 20
     figure = None
     
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.field = []
+        self.score = 0
+        self.state = "start"
+        for i in range(height):
+            new_line = []
+            for j in range(width):
+                new_line.append(0)
+            self.field.append(new_line)
+
+    def new_figure(self):
+        self.figure = Figure(3, 0)
+            

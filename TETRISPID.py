7cd60f4dabec4b6736914eb3edee466240a3472f
drdk98 <120593038+drdk98@users.noreply.github.com>
2023-01-10 13:45:37 -0500

    Update TETRISPID.py

--- TETRISPID.py
+++ TETRISPID.py
@@ -30,3 +30,17 @@ class Figure:
         [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
         [[1, 2, 5, 6]],
     ]
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
+
+    def image(self):
+        return self.figures[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
+        
